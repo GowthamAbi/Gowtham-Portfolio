eaa71aa7be26d5a1d572ddfbcaba34d7308eb755
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-05-21 23:24:55 +0530

    resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ export default function Resume() {
         <h2 className="text-4xl font-bold text-gray-800 mb-4">Resume</h2>
         <p className="text-lg text-gray-600 mb-6">Download my resume below</p>
         <a
-          href="/resume/GOWTHAM.pdf"
+          href="public\resume\GOWTHAM A.pdf"
           download="Gowtham_Resume.pdf"
           className="inline-block px-6 py-3 bg-indigo-600 text-white rounded-lg shadow-md hover:bg-indigo-700 transition duration-300"
         >
