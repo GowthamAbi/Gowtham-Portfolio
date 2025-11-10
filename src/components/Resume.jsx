e02086aa59a8481fc3056f541b89a63b5e406618
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-11-10 16:09:06 +0530

    b

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -9,7 +9,7 @@ export default function Resume() {
           className="inline-block px-6 py-3 bg-indigo-600 text-white rounded-lg shadow-md hover:bg-indigo-700 transition duration-300"
           
         >
-          Download Resume
+          Download Resume.
         </a>
       </section>
     );
